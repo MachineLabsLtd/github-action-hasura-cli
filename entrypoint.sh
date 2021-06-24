3b8331c918dd12260be325e907e4dadcb8e6a66d
Ben Ledbury <ben@machinelabs.com>
2021-06-24 19:04:17 +0100

    Add hasura engine version upgrade

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ if [[ -z "$HASURA_GRAPHQL_ENDPOINT" || -z "$HASURA_GRAPHQL_ADMIN_SECRET" || -z "
     exit 126
 fi
 
+if [ -n "$HASURA_ENGINE_VERSION" ]; then
+    hasura update-cli --version $HASURA_ENGINE_VERSION
+fi
+
 exec hasura $*
